9353eee08b0489a385bbe46359cca83812ef3d3d
momen-samirr <momensamir759@gmail.com>
2025-01-16 17:52:18 +0200

    fix create page

--- app/dashboard/banners/create/page.tsx
+++ app/dashboard/banners/create/page.tsx
@@ -1,113 +1,113 @@
-// "use client";
+"use client";
 
-// import { createBanner } from "@/app/actions";
-// import SubmitButtons from "@/app/components/dashboard/submitButtons";
-// import { UploadButton } from "@/app/lib/uploadthing";
-// import { bannerSchema } from "@/app/lib/zodSchema";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import { useForm } from "@conform-to/react";
-// import { parseWithZod } from "@conform-to/zod";
-// import { ChevronLeft } from "lucide-react";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { useActionState, useState } from "react";
+import { createBanner } from "@/app/actions";
+import SubmitButtons from "@/app/components/dashboard/submitButtons";
+import { UploadButton } from "@/app/lib/uploadthing";
+import { bannerSchema } from "@/app/lib/zodSchema";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { ChevronLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useActionState, useState } from "react";
 
-// export default function CreateBannerRoute() {
-//   const [image, setImage] = useState<string | undefined>(undefined);
-//   const [lastResult, action] = useActionState(createBanner, undefined);
-//   const [form, fileds] = useForm({
-//     lastResult,
-//     onValidate: ({ formData }) => {
-//       return parseWithZod(formData, {
-//         schema: bannerSchema,
-//       });
-//     },
-//     shouldValidate: "onBlur",
-//     shouldRevalidate: "onInput",
-//   });
+export default function CreateBannerRoute() {
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const [lastResult, action] = useActionState(createBanner, undefined);
+  const [form, fileds] = useForm({
+    lastResult,
+    onValidate: ({ formData }) => {
+      return parseWithZod(formData, {
+        schema: bannerSchema,
+      });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
-//   return (
-//     <>
-//       <form id={form.id} onSubmit={form.onSubmit} action={action}>
-//         <div className="flex items-center gap-5">
-//           <Button variant={"outline"} size={"icon"} asChild>
-//             <Link href={"/dashboard/banners"}>
-//               <ChevronLeft className="size-5" />
-//             </Link>
-//           </Button>
-//           <h1 className="text-xl font-semibold tracking-tight">
-//             Create Banner
-//           </h1>
-//         </div>
-//         <Card className="mt-5">
-//           <CardHeader>
-//             <CardTitle>Banner Form</CardTitle>
-//             <CardDescription>
-//               This is a form to create a new banner
-//             </CardDescription>
-//           </CardHeader>
-//           <CardContent>
-//             <div className="flex flex-col gap-6">
-//               <div className="flex flex-col gap-3">
-//                 <Label>Title</Label>
-//                 <Input
-//                   type="text"
-//                   placeholder="Enter title"
-//                   key={fileds.title.key}
-//                   name={fileds.title.name}
-//                   defaultValue={fileds.title.initialValue}
-//                 />
-//                 <p className="text-red-500">{fileds.title.errors}</p>
-//               </div>
-//               <div className="flex flex-col gap-3">
-//                 <Label>Image</Label>
-//                 <input
-//                   type="hidden"
-//                   value={image}
-//                   key={fileds.imageString.key}
-//                   name={fileds.imageString.name}
-//                   defaultValue={fileds.imageString.initialValue as string}
-//                 />
-//                 {image !== undefined ? (
-//                   <div className="flex items-center gap-5">
-//                     <Image
-//                       src={image}
-//                       alt="image"
-//                       width={200}
-//                       height={200}
-//                       className=" w-[200px] h-[200px] rounded-md"
-//                     />
-//                   </div>
-//                 ) : (
-//                   <UploadButton
-//                     endpoint={"imageUploader"}
-//                     onClientUploadComplete={(res) => {
-//                       setImage(res[0].url);
-//                     }}
-//                     onUploadError={(error: Error) => {
-//                       console.error(error);
-//                     }}
-//                   />
-//                 )}
-//                 <p className="text-red-500">{fileds.imageString.errors}</p>
-//               </div>
-//             </div>
-//           </CardContent>
-//           <CardFooter>
-//             <SubmitButtons text="Create Banner" />
-//           </CardFooter>
-//         </Card>
-//       </form>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <form id={form.id} onSubmit={form.onSubmit} action={action}>
+        <div className="flex items-center gap-5">
+          <Button variant={"outline"} size={"icon"} asChild>
+            <Link href={"/dashboard/banners"}>
+              <ChevronLeft className="size-5" />
+            </Link>
+          </Button>
+          <h1 className="text-xl font-semibold tracking-tight">
+            Create Banner
+          </h1>
+        </div>
+        <Card className="mt-5">
+          <CardHeader>
+            <CardTitle>Banner Form</CardTitle>
+            <CardDescription>
+              This is a form to create a new banner
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col gap-3">
+                <Label>Title</Label>
+                <Input
+                  type="text"
+                  placeholder="Enter title"
+                  key={fileds.title.key}
+                  name={fileds.title.name}
+                  defaultValue={fileds.title.initialValue}
+                />
+                <p className="text-red-500">{fileds.title.errors}</p>
+              </div>
+              <div className="flex flex-col gap-3">
+                <Label>Image</Label>
+                <input
+                  type="hidden"
+                  value={image}
+                  key={fileds.imageString.key}
+                  name={fileds.imageString.name}
+                  defaultValue={fileds.imageString.initialValue as string}
+                />
+                {image !== undefined ? (
+                  <div className="flex items-center gap-5">
+                    <Image
+                      src={image}
+                      alt="image"
+                      width={200}
+                      height={200}
+                      className=" w-[200px] h-[200px] rounded-md"
+                    />
+                  </div>
+                ) : (
+                  <UploadButton
+                    endpoint={"imageUploader"}
+                    onClientUploadComplete={(res) => {
+                      setImage(res[0].url);
+                    }}
+                    onUploadError={(error: Error) => {
+                      console.error(error);
+                    }}
+                  />
+                )}
+                <p className="text-red-500">{fileds.imageString.errors}</p>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <SubmitButtons text="Create Banner" />
+          </CardFooter>
+        </Card>
+      </form>
+    </>
+  );
+}
--- app/dashboard/banners/page.tsx
+++ app/dashboard/banners/page.tsx
@@ -1,104 +1,104 @@
-// import prisma from "@/app/lib/db";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuSeparator,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/table";
-// import { MoreHorizontalIcon, PlusCircle } from "lucide-react";
-// import Image from "next/image";
-// import Link from "next/link";
+import prisma from "@/app/lib/db";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { MoreHorizontalIcon, PlusCircle } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-// export const getBannerData = async () => {
-//   const res = await prisma.banner.findMany({
-//     orderBy: {
-//       createdAt: "desc",
-//     },
-//   });
-//   return res;
-// };
+export const getBannerData = async () => {
+  const res = await prisma.banner.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return res;
+};
 
-// export default async function BannersPage() {
-//   const data = await getBannerData();
-//   return (
-//     <>
-//       <div className="flex items-center justify-end">
-//         <Button asChild>
-//           <Link href={"/dashboard/banners/create"}>
-//             <PlusCircle className="size-5" />
-//             <span>Create Banner</span>
-//           </Link>
-//         </Button>
-//       </div>
-//       <Card className="mt-5">
-//         <CardHeader>
-//           <CardTitle>Banners Page</CardTitle>
-//           <CardDescription>This is a list of all the Banners.</CardDescription>
-//         </CardHeader>
-//         <CardContent>
-//           <Table>
-//             <TableHeader>
-//               <TableRow>
-//                 <TableHead>Image</TableHead>
-//                 <TableHead>Title</TableHead>
-//                 <TableHead className="text-right">Actions</TableHead>
-//               </TableRow>
-//             </TableHeader>
-//             <TableBody>
-//               {data.map((banner) => (
-//                 <TableRow key={banner.id}>
-//                   <TableCell>
-//                     <Image
-//                       src={banner.imageString}
-//                       alt="image"
-//                       width={100}
-//                       height={100}
-//                       className="relative rounded-full"
-//                     />
-//                   </TableCell>
-//                   <TableCell>{banner.title}</TableCell>
-//                   <TableCell className="text-right">
-//                     <DropdownMenu>
-//                       <DropdownMenuTrigger asChild>
-//                         <Button variant={"ghost"}>
-//                           <MoreHorizontalIcon />
-//                         </Button>
-//                       </DropdownMenuTrigger>
-//                       <DropdownMenuContent>
-//                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-//                         <DropdownMenuSeparator />
-//                         <DropdownMenuItem asChild>
-//                           <Link href={`/dashboard/banners/${banner.id}/delete`}>
-//                             Delete
-//                           </Link>
-//                         </DropdownMenuItem>
-//                       </DropdownMenuContent>
-//                     </DropdownMenu>
-//                   </TableCell>
-//                 </TableRow>
-//               ))}
-//             </TableBody>
-//           </Table>
-//         </CardContent>
-//       </Card>
-//     </>
-//   );
-// }
+export default async function BannersPage() {
+  const data = await getBannerData();
+  return (
+    <>
+      <div className="flex items-center justify-end">
+        <Button asChild>
+          <Link href={"/dashboard/banners/create"}>
+            <PlusCircle className="size-5" />
+            <span>Create Banner</span>
+          </Link>
+        </Button>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Banners Page</CardTitle>
+          <CardDescription>This is a list of all the Banners.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data.map((banner) => (
+                <TableRow key={banner.id}>
+                  <TableCell>
+                    <Image
+                      src={banner.imageString}
+                      alt="image"
+                      width={100}
+                      height={100}
+                      className="relative rounded-full"
+                    />
+                  </TableCell>
+                  <TableCell>{banner.title}</TableCell>
+                  <TableCell className="text-right">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant={"ghost"}>
+                          <MoreHorizontalIcon />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                          <Link href={`/dashboard/banners/${banner.id}/delete`}>
+                            Delete
+                          </Link>
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
