396f45023d18e16d62dd2feeed73473473c68eb8
momen-samirr <momensamir759@gmail.com>
2025-01-16 21:13:46 +0200

    fix this29

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,7 +10,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  getKindeServerSession,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export default async function DashboardLayout({
@@ -18,6 +21,9 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <div className="flex w-full flex-col max-w-7xl mx-auto px-5 sm:px-6 lg:px-8">
       <header className="sticky top-0 h-16 gap-6 flex items-center justify-between bg-white border-b">
@@ -49,7 +55,11 @@ export default async function DashboardLayout({
               <Avatar>
                 <AvatarImage
                   className="w-full h-full rounded-full object-cover"
-                  src={"https://avatar.iran.liara.run/public/7"}
+                  src={
+                    user.picture
+                      ? user.picture
+                      : "https://avatar.iran.liara.run/public/7"
+                  }
                   alt="Image"
                 />
               </Avatar>
