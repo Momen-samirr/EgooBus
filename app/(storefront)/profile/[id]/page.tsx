33a54d028b920477e24da1a65ef880c319406d44
momen-samirr <momensamir759@gmail.com>
2025-01-17 20:59:24 +0200

    Add Apllication number

--- app/(storefront)/profile/[id]/page.tsx
+++ app/(storefront)/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ import { ProfilePage } from "@/app/components/storefront/EditAppNumber";
 import prisma from "@/app/lib/db";
 import { notFound } from "next/navigation";
 
-export const getUserData = async (userId: string) => {
+const getUserData = async (userId: string) => {
   const userData = await prisma.user.findUnique({
     where: {
       id: userId,
